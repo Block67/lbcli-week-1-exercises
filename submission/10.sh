1fd05a9a062aa21cd2e3e2ab6de933a9525bc02f
Block67 <block6703@gmail.com>
2026-03-17 11:49:55 +0100

    fix: perfect output format for exercise 10

--- submission/10.sh
+++ submission/10.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 # Check if the address is valid. 
-# The test expects "true".
-bitcoin-cli -regtest validateaddress "bcrt1qckgvfee4qs6y98jrcn8qc0m6ce6sxls0vac3yy" | jq -r .isvalid | tr -d '\n\r '
-echo ""
+# We use grep instead of jq for better compatibility in the test runner.
+bitcoin-cli -regtest validateaddress bcrt1qckgvfee4qs6y98jrcn8qc0m6ce6sxls0vac3yy | grep -q '"isvalid": true' && echo -n "true" || echo -n "false"
