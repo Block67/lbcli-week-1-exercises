71c232bfcb85ec8560f836327ae78c6de6d8a0d3
Block67 <block6703@gmail.com>
2026-03-17 11:56:29 +0100

    fix: exercise 10

--- submission/10.sh
+++ submission/10.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Check if the address is valid. 
-# We use grep instead of jq for better compatibility in the test runner.
-bitcoin-cli -regtest validateaddress bcrt1qckgvfee4qs6y98jrcn8qc0m6ce6sxls0vac3yy | grep -q '"isvalid": true' && echo -n "true" || echo -n "false"
+# Check if the following address is valid or invalid. Return only `true` or `false`
+bitcoin-cli -regtest validateaddress "bcrt1qckgvfee4qs6y98jrcn8qc0m6ce6sxls0vac3yy" 2>/dev/null | jq -r .isvalid
