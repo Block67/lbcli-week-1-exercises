f07b9172760994fa5aed00ed832df401f2d1b64d
Block67 <block6703@gmail.com>
2026-03-17 11:59:49 +0100

    fix: exercise 10

--- submission/10.sh
+++ submission/10.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-# Check if the following address is valid or invalid. Return only `true` or `false`
-bitcoin-cli -regtest validateaddress "bcrt1qckgvfee4qs6y98jrcn8qc0m6ce6sxls0vac3yy" 2>/dev/null | jq -r .isvalid
+# Check if the following address is valid or invalid. Return only `true` or `false` - 
+# Address: `bcrt1qckgvfee4qs6y98jrcn8qc0m6ce6sxls0vac3yy`
+
+BTC_ADDRESS="bcrt1qckgvfee4qs6y98jrcn8qc0m6ce6sxls0vac3yy"
+bitcoin-cli -regtest validateaddress "$BTC_ADDRESS" | jq -r ".isvalid"
